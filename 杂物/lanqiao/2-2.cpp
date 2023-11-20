ebd91c47aae7171206b79f6cc0217875a75781a3
alex1263 <1685705754@gmail.com>
2023-11-20 23:51:54 +0800

    修改蓝桥杯题目代码

--- 杂物/lanqiao/2-2.cpp
+++ 杂物/lanqiao/2-2.cpp
@@ -1,19 +1,24 @@
-#include <cstdio>
 #include <iostream>
 #include <cmath>
 using namespace std;
 
-int mian()
+double n = 23333333, ans = 11625907.5798;
+
+int main()
 {
-    long long x = 1, n = 23333333, y;
-    double ans = 11625907.5798, sum = 0;
-    for (x = 1; x <= n / 2 + 1; x++)
+    double x, y, x1, y1, sum;
+
+    for (x = 1; x <= n / 2; x++)
     {
         y = n - x;
-        sum = double(x * x / n) * (log2(double(x / n))) + double(y * y / n) * (log2(double(y / n)));
+        sum = -(x * x / n) * (log2(x) - log2(n)) - (y * y / n) * (log2(y) - log2(n));
+        // double sum舍去四位小数之后的数,不用四舍五入
+        sum = floor(sum * 10000) / 10000;
+
         if (sum == ans)
         {
-            cout << x << endl;
+            printf("%.4lf", sum);
+            printf(" %.0lf %.0lf\n", x, y);
             break;
         }
     }
