45e190e7f8c06b2aeb5421487f09610d0d7924db
alex1263 <q1685705754@gmail.com>
2024-03-08 15:13:31 +0800

    增加高精度加法

--- OJ/洛谷/C++/P1255.cpp
+++ OJ/洛谷/C++/P1255.cpp
@@ -2,19 +2,34 @@
 
 using namespace std;
 
-int n = 0, ans[5005] = {0};
+int n = 0, ans[5005][5005] = {0};
 
 int main()
 {
-    ans[0] = 0;
-    ans[1] = 1;
-    ans[2] = 2;
+    int len = 1;
+    ans[0][1] = 0;
+    ans[1][1] = 1;
+    ans[2][1] = 2;
     cin >> n;
-    for (long long i = 3; i <= n; i++)
+    // 高精度加法
+    for (int i = 3; i <= n; i++)
     {
-        ans[i] = ans[i - 1] + ans[i - 2];
+        for (int j = 1; j <= len; j++)
+        {
+            ans[i][j] = ans[i][j] + ans[i - 1][j] + ans[i - 2][j];
+            ans[i][j + 1] = ans[i][j] / 10;
+            ans[i][j] = ans[i][j] % 10;
+        }
+        if (ans[i][len + 1] > 0)
+        {
+            len++;
+        }
     }
-    cout << ans[n] << endl;
+
+    for (int i = len; i >= 1; i--)
+    {
+        cout << ans[n][i];
+    }
 
     return 0;
 }
